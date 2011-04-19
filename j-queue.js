3fc1d89875491b87819c3011cf535f27790ce2ed
Dominic Tarr <dominic.tarr@gmail.com>
2011-04-19 13:16:50 +1000

    initial

---
+++ j-queue.js
@@ -0,0 +1,77 @@
+
+var http = require('http')
+
+module.exports = Queue
+
+function Queue (options){
+  this.__options = options  = options || {}
+  if(!options.method)
+    options.method = 'POST'
+//  this.__conn = this.connect()
+}
+
+Funx.prototype = []
+function Funx (){
+  if(!(this instanceof Funx)) return new Funx()
+  var arguments
+  this.apply = function (other,args){
+    this.forEach(function (e){
+      e.apply(other,args)
+    })
+  }
+  this.call = function (){
+    var other = [].shift.call(arguments)
+      , args = arguments
+    this.forEach(function (e){
+      e.apply(other,args)
+    })
+  }
+  this.remove = function(func){
+    delete this[this.indexOf(func)]
+  }
+}
+
+Queue.prototype = {
+  send: function (){
+    var a = [], self = this
+    while(arguments.length)
+      a.push([].shift.call(arguments))
+
+    this.__messages = this.__messages || []
+    this.__messages.push(a)
+    console.log('send:',a)
+//    if(this.__req)
+//    this.__req.abort()
+
+    if(this.__timer)//defur sending message, incase send gets called again syncronously.
+      clearTimeout(this.__timer)
+    this.__timer = setTimeout(function (){
+      self.connect()
+    },0)
+  }
+, __receivers: new Funx()
+, __drains: new Funx()
+, receive: function (f){this.__receivers.push(f); return this}
+, drain: function (f){this.__drains.push(f); return this}
+, connect: function (){
+
+    var self = this
+    var req = this.__req = http.request(this.__options,function (res){
+      data = ''
+      res.on('data',function (e){
+        data += e
+      })
+      res.on('end', function (){
+        JSON.parse(data).forEach(function (e){
+          self.__receivers.apply(null,e)
+        })
+        console.log("DRAQIN!",self.__drains)
+        self.__drains.call()
+      })
+    })
+
+    console.log('connect:',this.__messages)
+    req.end(JSON.stringify(this.__messages))
+    self.__messages = []
+  }
+}
---
+++ test/j-queue.asynct.js
@@ -0,0 +1,92 @@
+
+var Jq = require('../j-queue')
+  , it = require('it-is')
+  , http = require('http')
+  , echo
+  , port = 1234
+  , request = require('request')
+
+exports.__setup = function (test){
+  var testMessage = 'dsfhaskfjhaskfdjhaskdfhjakscnjkasjcn'
+
+  echo = http.createServer(function (req,res){
+
+    res.writeHead(200,{
+      'Content-Type': 'application/json'
+    })
+
+    req.on('data',function (e){
+      //console.log('got:' + e)
+      res.write(e)
+    })
+
+    req.on('end',function () {res.end()})
+
+  })
+
+  echo.listen(port, checkSetup)
+
+  function checkSetup (){
+
+  request({uri: 'http://localhost:' + port, method:'POST', body:testMessage},function (err,res,body){
+      if(err)
+        return test.error(err)
+      console.log('body:',body)
+      it(body).equal(testMessage)
+      console.log('test.done')
+
+      test.done()
+    })
+  }
+
+}
+exports.__teardown = function (test){
+
+//  if(echo)
+    echo.close()
+//  test.done()
+}
+
+/*exports [ 'test api' ] = function (test){
+  it(Jq).function()
+  var jq = new Jq()
+
+  it(jq).has({
+    send: it.function ()
+  , recieve: it.function ()
+  , drain: it.function ()
+  })
+
+  test.done()
+}
+
+//*/
+
+
+
+exports ['send messages'] = function (test){
+
+  var jq = new Jq({port: port})
+    , messages = [1,2,3,4,5,6,7,8]
+    , i = 0
+
+  //make a server which echos commands
+
+  jq.receive(function (e){
+    console.log('>>>',e)
+    it(e).equal(messages[i++])
+  })
+  jq.drain(test.done) // called when there are no more messages coming...
+
+  messages.forEach(function (e){jq.send(e)})
+
+/*  var j = 0
+  function next () {
+
+    jq.send(messages[j++])
+    if(j < messages.length)
+    setTimeout(next,1e3)
+  }
+  next()*/
+}
+
